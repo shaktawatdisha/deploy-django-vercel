22bcbdb58020642f10292cb409a3d4b9f8e4e461
shaktawatdisha <shaktawatdisha7@gmail.com>
2025-01-13 15:57:25 +0530

    removed collect static

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# import os
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
