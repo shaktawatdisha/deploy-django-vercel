739d2a30cc836d4f84137da7ce0e220b50dfc6cd
shaktawatdisha <shaktawatdisha7@gmail.com>
2025-01-13 16:17:10 +0530

    added again static files

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -119,12 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build",'static')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# import os
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
